7660504ed94b5855d275ed1f9aca00e93a4fdf94
kunhao-Zhu <73526253+kunhao-Zhu@users.noreply.github.com>
2025-11-10 04:15:50 +0000

    Deploying to gh-pages from @ kunhao-Zhu/kunhao-zhu.github.io@1a0a580244164270e4aa61c6fb5d34f823f453ef 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "Curriculum Vitae of Kunhao Zhu — digital humanities, knowledge organization, spatial–narrative modeling.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
@@ -459,14 +459,14 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%6B%68.%7A%68%75@%65%6D%61%69%6C.%6E%6A%75.%65%64%75.%63%6E", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-orcid',
+        title: 'ORCID',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://orcid.org/0009-0007-2679-8486", "_blank");
         },
       },{
         id: 'social-rss',
@@ -474,20 +474,6 @@ ninja.data = [{
         section: 'Socials',
         handler: () => {
           window.open("/feed.xml", "_blank");
-        },
-      },{
-        id: 'social-scholar',
-        title: 'Google Scholar',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
         },
       },{
       id: 'light-theme',
